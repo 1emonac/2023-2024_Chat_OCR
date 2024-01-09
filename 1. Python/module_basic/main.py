6a74daa91a51c63fed95d755dee255861292be64
1emonac <139857418+1emonac@users.noreply.github.com>
2024-01-09 16:56:21 +0900

    Add files via upload

---
+++ 1. Python/module_basic/main.py
@@ -0,0 +1,11 @@
+import test_module as test
+
+radius = test.number_input()
+2
+print(test.get_circumference(radius))
+print(test.get_circle_area(radius))
+
+# print("메인의 __name__을 출력하기")
+# print(__name__)
+# print()
+
